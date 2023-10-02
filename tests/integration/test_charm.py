2afa230bd24913efa7b8a8bb06c55e8f5c516097
arturo-seijas <arturo.seijas@canonical.com>
2023-10-02 18:19:45 +0200

    fix integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -16,12 +16,7 @@ async def test_active(ops_test: OpsTest, app: ops.Application):
 
     Assume that the charm has already been built and is running.
     """
-    await app.set_config(  # type: ignore[attr-defined]
-        {
-            "host": "smtp.example",
-            "port": 25,
-        }
-    )
+    await app.set_config({"host": "smtp.example"})  # type: ignore[attr-defined]
     status_name = ops.ActiveStatus.name  # type: ignore[has-type]
     assert ops_test.model
     await ops_test.model.wait_for_idle(status=status_name, raise_on_error=True)
@@ -40,12 +35,7 @@ async def test_legacy_relation(
     relation_name = f"{app.name}:smtp_legacy"
     assert ops_test.model
     await ops_test.model.add_relation(any_charm.name, relation_name)
-    await app.set_config(  # type: ignore[attr-defined]
-        {
-            "host": "smtp.example",
-            "port": 25,
-        }
-    )
+    await app.set_config({"host": "smtp.example"})  # type: ignore[attr-defined]
     status_name = ops.ActiveStatus.name  # type: ignore[has-type]
     await ops_test.model.wait_for_idle(status=status_name, raise_on_error=True)
     assert app.units[0].workload_status == status_name  # type: ignore
