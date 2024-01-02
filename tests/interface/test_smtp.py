10cddac8efcd16323683baec49d16f2064c2f4c4
arturo-seijas <arturo.seijas@canonical.com>
2024-01-02 16:59:05 +0100

    remove unused configs

--- tests/interface/test_smtp.py
+++ tests/interface/test_smtp.py
@@ -12,8 +12,6 @@ def test_smtp_v0_interface(interface_tester: InterfaceTester):
         interface_tester: interface tester.
     """
     interface_tester.configure(
-        repo="https://github.com/canonical/charm-relation-interfaces",
-        branch="main",
         juju_version="3.4",
         interface_name="smtp",
         interface_version=0,
