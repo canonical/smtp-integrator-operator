2fb867890bb71d2977521e310eddd944801795e6
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2024-01-03 11:22:33 +0100

    Interface tests (#31)

---
+++ tests/interface/test_smtp.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""SMTP interface tests."""
+from interface_tester import InterfaceTester
+
+
+def test_smtp_v0_interface(interface_tester: InterfaceTester):
+    """Test smtp interface.
+
+    Args:
+        interface_tester: interface tester.
+    """
+    interface_tester.configure(
+        juju_version="3.4",
+        interface_name="smtp",
+        interface_version=0,
+    )
+    interface_tester.run()
