48320ce733775971b1118f1d4e3877c996490f5e
Weii Wang <weii.wang@canonical.com>
2024-02-14 18:09:49 +0800

    Add PYDEPS and upgrade pydantic to v2

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -53,6 +53,7 @@ async def any_charm(ops_test: OpsTest):
         "any-charm",
         application_name="any",
         channel="beta",
-        config={"src-overwrite": json.dumps(src_overwrite)},
+        # Sync the python-packages here with smtp charm lib PYDEPS
+        config={"src-overwrite": json.dumps(src_overwrite), "python-packages": "pydantic>=2"},
     )
     yield application
