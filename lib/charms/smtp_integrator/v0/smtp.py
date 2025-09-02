90cbfea67ed3d855a164697e5abb5e0342ef4f25
Arturo Seijas <arturo.seijas@canonical.com>
2025-09-02 13:00:28 +0200

    Fix linting

--- lib/charms/smtp_integrator/v0/smtp.py
+++ lib/charms/smtp_integrator/v0/smtp.py
@@ -367,14 +367,12 @@ class SmtpRequires(ops.Object):
             if self._is_relation_data_valid(event.relation):
                 self.on.smtp_data_available.emit(event.relation, app=event.app, unit=event.unit)
     
-    def _on_secret_changed(self, event: ops.SecretChanged) -> None:
+    def _on_secret_changed(self, event: ops.SecretChangedEvent) -> None:
         """Handle the relation secret event.
 
         Args:
             event: event triggering this handler.
         """
-        if not self.charm.unit.is_leader():
-            return
         if event.secret.label != PASSWORD_SECRET_LABEL:
             return
         self.on.smtp_data_available.emit(app=event.app, unit=event.unit)
